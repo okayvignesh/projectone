1a704d415c4166cd0df5faa83c6294da26721bc8
Vignesh <vignesh@kandradigital.com>
2023-08-12 13:24:46 +0530

    added login api

--- backend/routes/user.controller.js
+++ backend/routes/user.controller.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router();
 const dotenv = require('dotenv');
 const User = require('../models/user');
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 dotenv.config();
 
 
@@ -14,21 +17,25 @@ router.get('/', async (req, res) => {
     res.send('This is  Users 🦸')
 })
 
+
+
 router.post('/register', async (req, res) => {
     try {
-        const { username } = req.body;
         let success = false
-        const existingUser = await User.findOne({ username });
+        const existingUser = await User.findOne({ username: req.body.username });
 
         if (existingUser) {
             return res.status(409).json({ success: false, message: 'Username already exists ❌' });
         }
 
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt)
+
         const NewUser = new User({
-            username: req.body.data.username,
-            firstname: req.body.data.firstname,
-            lastname: req.body.data.lastname,
-            password: req.body.data.password
+            username: req.body.username,
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            password: hashedPassword
         })
 
         const savedUser = await NewUser.save();
@@ -38,6 +45,34 @@ router.post('/register', async (req, res) => {
         res.status(500).json({ error: error.message })
     }
 })
+
+
+
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+
+        if (!user) {
+            return res.status(400).send("User does not Exist!");
+        }
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        if (!validPassword) {
+            return res.status(400).send("Incorrect Password!");
+        }
+
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const Token = jwt.sign(data, process.env.SECRET_KEY);
+        res.status(200).json({ success: true, message: 'Login Successful', Token });
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
+    }
+});
+
 
 
 
